7e9e16141e332094a681764f48227ce76d86c267
Dave Gomez <Dave Gomez>
2024-03-20 11:32:34 -0300

    relaciones

--- src/database/models/page.ts
+++ src/database/models/page.ts
@@ -1,12 +1,19 @@
 import { DataTypes, Model } from 'sequelize';
 import { PageInterface } from '../../models/page';
 import { sequelize } from '../database';
+import { Section } from './section';
 
-const Page = sequelize.define<Model, PageInterface>('pages', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+const Page = sequelize.define<Model, PageInterface>(
+    'pages',
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-});
+    { paranoid: true }
+);
+
+Page.hasMany(Section);
 
 export { Page };
--- src/database/models/section.ts
+++ src/database/models/section.ts
@@ -1,6 +1,9 @@
 import { DataTypes, Model } from 'sequelize';
 import { SectionInterface } from '../../models/section';
 import { sequelize } from '../database';
+import { Image } from './image';
+import { Text } from './text';
+import { Title } from './title';
 
 const Section = sequelize.define<Model, SectionInterface>('section', {
     name: {
@@ -8,5 +11,9 @@ const Section = sequelize.define<Model, SectionInterface>('section', {
         allowNull: false,
     },
 });
+
+Section.hasMany(Text);
+Section.hasMany(Title);
+Section.hasMany(Image);
 
 export { Section };
