082fb23ad3533dc953e52c9e48dcefa62c205a53
Dave Gomez <Dave Gomez>
2024-03-23 12:16:06 -0300

    entpoints para paginas listos

--- src/routes/page.ts
+++ src/routes/page.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { readPages, readpageById } from '../database/controllers/pageController';
+import { createPage, deletePage, readPages, readpageById, updatePage } from '../database/controllers/pageController';
 import { checkSession } from '../database/middlewares/auth';
 
 const router = Router();
 
 router.get('/', readPages);
 router.get('/:id', readpageById);
-router.post('/', checkSession, readpageById);
+router.post('/', checkSession, createPage);
+router.put('/', checkSession, updatePage);
+router.delete('/', checkSession, deletePage);
 
 export { router };
