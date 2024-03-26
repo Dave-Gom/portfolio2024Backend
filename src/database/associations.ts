e38471a54ca0933ea258545bbcfac1a196caae1e
Dave Gomez <Dave Gomez>
2024-03-25 21:36:09 -0400

    asociaciones

--- src/database/associations.ts
+++ src/database/associations.ts
@@ -1,7 +1,22 @@
 import { Language } from './models/language';
+import { Page } from './models/page';
+import { Section } from './models/section';
 import { Text } from './models/text';
+import { Title } from './models/title';
 
 export const associate = () => {
     Language.hasMany(Text);
     Text.belongsTo(Language);
+
+    Language.hasMany(Title);
+    Title.belongsTo(Language);
+
+    Section.hasMany(Text);
+    Text.belongsTo(Section);
+
+    Section.hasMany(Title);
+    Title.belongsTo(Section);
+
+    Page.hasMany(Section);
+    Section.belongsTo(Page);
 };
